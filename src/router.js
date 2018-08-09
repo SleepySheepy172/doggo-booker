43a5709c2ca6550a11b1db148d3cf8f4d397fa5b
Art <33687551+MissArray@users.noreply.github.com>
2018-08-09 11:13:39 +0100

    added make-booking handler refer > #2

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-const { servePublicFile } = require('./handler');
+const { servePublicFile, makeBooking } = require('./handler');
 const path = require('path');
 
 const router = (req, res) => {
@@ -8,6 +8,9 @@ const router = (req, res) => {
   if (req.url === '/about') {
     servePublicFile(res, 'about.html');
   } 
+  if (req.url === '/make-booking') {
+    makeBooking(req, res);
+  }
   else {
     servePublicFile(res, req.url);
   }
