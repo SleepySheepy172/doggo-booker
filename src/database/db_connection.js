9d671e1a7eb2fa1c22fffd4a3867dbdddc8964a1
Emma Ogden <ecogden@gmail.com>
2018-08-09 18:16:19 +0100

    adds heroku database to .env file

--- src/database/db_connection.js
+++ src/database/db_connection.js
@@ -1,17 +1,20 @@
 // connect to the database
 const { Pool } = require('pg');
-
 const url = require('url');
+require('env2')('./config.env');
 
-if (!process.env.DB_URL) {
-  console.log('setting DB_URL from config.env');
-  require('env2')('./config.env');
-}
+
+const DB_URL = process.env.USERS_DB_URL;
+
+// if (!DB_URL) {
+//   console.log('setting DB_URL from config.env');
+//   require('env2')('./config.env');
+// }
 
 // check if database url always exists
-if (!process.env.DB_URL) throw new Error('Environment variable DB_URL must be set');
+if (!DB_URL) throw new Error('Environment variable DB_URL must be set');
 
-const params = url.parse(process.env.DB_URL);
+const params = url.parse(DB_URL);
 const [username, password] = params.auth.split(':');
 
 const options = {
