4bb34ca30922eeba39e0c7e7b36d7ac629d83c6a
Michael Watts <hello@michaelwatts.co>
2018-08-10 13:21:25 +0100

    add form validation

--- public/js/DOM.js
+++ public/js/DOM.js
@@ -37,12 +37,15 @@ function addListItem(parent, text) {
   parent.appendChild(li);
 }
 
+var bookinglist = {};
+
 function getBookings(start, end) {
   url = '/get-bookings?start=' + start + '&end=' + end;
   request(url, function(err, data){
     if (err) return console.log(err);
     removeChildren('bookings-list');
     var bookings = JSON.parse(data);
+    bookingState = bookings;
     if (bookings.length) {
     bookings.forEach(function(booking){
       var item = document.createElement('li');
@@ -66,20 +69,77 @@ function addFormElement(parent, id, type, name, value, placeholder) {
   element.id = id;
   element.name = name;
   element.value = value;
+  element.required = true;
   if (placeholder !== undefined) element.placeholder = placeholder;
   parent.appendChild(element);
+}
+
+function makeBooking(querystring) {
+  var XHR = new XMLHttpRequest();
+
+  // Define what happens on successful data submission
+  XHR.addEventListener('load', function(event) {
+    alert('Yeah! Data sent and response loaded.');
+  });
+
+  // Define what happens in case of error
+  XHR.addEventListener('error', function(event) {
+    alert('Oops! Something went wrong.');
+  });
+
+  // Set up our request
+  XHR.open('POST', '/make-booking');
+
+  // Send our FormData object; HTTP headers are set automatically
+  XHR.send(querystring);
+}
+
+function validateForm(name, contact, startTime, endTime) {
+  //check that name is alphabetical
+  var nameRegex = /[^a-zA-Z ]/;
+  if (nameRegex.test(name)) {
+    alert('Please enter alphabetical characters only in name');
+    return false;
+  }
+  //check that phone number is valid
+  if (contact.indexOf(0) !== 0 || contact.length !== 11) {
+    alert('please enter a valid UK phone number');
+    return false;
+  }
+  // check that requested booking does not overlap existing booking
+  bookingState.forEach(function(booking){
+    console.log(booking);
+    if (startTime >= booking.start_time && startTime<= booking.end_time) {
+      alert('Please select an available start time')
+      return false
+    } else if (endTime >= booking.start_time && endTime <= booking.end_time) {
+      alert('Please select an available end time')
+      return false
+    }
+  })
+  // check that end time is after start time
+  if (startTime > endTime) {
+    alert('Please choose an end time after your start time');
+    return false;
+  }
+  if (startTime < '10:00:00' || endTime > '18:00:00') {
+    alert('Mackerel is only available between 10am and 6pm');
+    return false;
+  }
+  return true;
 }
 
 function renderForm(date) {
   removeChildren('booking-form');
   var form = document.createElement('form');
+  form.id = 'form'
   form.classList.add('form');
   form.action = '/make-booking';
   form.method = 'POST';
   bookingForm.appendChild(form);
   addFormElement(form, 'form-date', 'hidden', 'date', date);
   addFormElement(form, 'name', 'text', 'name', '', 'Snoopy');
-  addFormElement(form, 'contact', 'text', 'contact', '', '07654321012');
+  addFormElement(form, 'contact', 'number', 'contact', '', '07654321012');
   addFormElement(form, 'start-time', 'time', 'start-time', '');
   addFormElement(form, 'end-time', 'time', 'end-time', '');
   var submit = document.createElement('button');
@@ -88,11 +148,20 @@ function renderForm(date) {
   submit.id = 'submit';
   submit.innerText = 'Submit';
   form.appendChild(submit);
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+    var date = document.getElementById('form-date').value;
+    var name = document.getElementById('name').value;
+    var contact = document.getElementById('contact').value;
+    var startTime = document.getElementById('start-time').value;
+    var endTime = document.getElementById('end-time').value;
+    if (validateForm(name, contact, startTime, endTime)) {
+      var data = 'date=' + date + '&name=' + name + '&contact=' + contact + '&start-time=' + startTime + '&end-time=' + endTime;
+      makeBooking(data);
+    }
+  });
 }
 
-// pick up database data on load
-//   [ { start_time: 2018-08-11T13:46:57.417Z,
-//        end_time: 2018-08-11T14:47:55.129Z } ]
 window.addEventListener("load", function (e) {
   console.log('I have loaded')
   // grab data here
