fa529cc53d59ffd04e66b61cbff0d24472bd0a16
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-08-16 11:28:02 +0100

    updated getBookings relates #9

--- src/queries/getBookings.js
+++ src/queries/getBookings.js
@@ -1,7 +1,7 @@
 const dbConnection = require('../database/db_connection');
 
 const getBookings = (start, end, cb) => {
-  dbConnection.query('SELECT users.first_name, users.last_name, bookings.start_time::time, bookings.end_time::time FROM bookings WHERE start_time >= $1 AND end_time <= $2 AND booking = true INNER JOIN users ON bookings.user_id = users.id ORDER BY start_time ASC;', [start, end], (err, data) => {
+  dbConnection.query('SELECT users.first_name, users.last_name, bookings.start_time::time, bookings.end_time::time FROM bookings INNER JOIN users ON bookings.user_id = users.id WHERE bookings.start_time >= $1 AND bookings.end_time <= $2 AND bookings.booking = true ORDER BY bookings.start_time ASC;', [start, end], (err, data) => {
     if (err) return cb(err);
     console.log('this is data.rows', data.rows)
     cb(null, data.rows);
